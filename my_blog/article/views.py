c9dca807f3de7cd4c33ebd3c4190ea56596d3071
CNA0086 <51186528+CNA0086@users.noreply.github.com>
2022-06-27 17:13:09 -0700

    Feature: adding client-side article delete
    Adding article update and edit function after posting on article detail page.

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -55,3 +55,19 @@ def article_safe_delete(request, id):
         return redirect("article:article_list")
     else:
         return HttpResponse("Only allow POST request.")
+
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("Error found in form, please fill it again.")
+    else:
+        article_post_form = ArticlePostForm()
+        context = { 'article': article, 'article_post_form': article_post_form }
+        return render(request, 'article/update.html', context)
